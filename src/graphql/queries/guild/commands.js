3eec170657cd1007df1f600aa2a460436d240631
pedall <peterdodell@icloud.com>
2018-10-04 00:54:27 +0200

    trying to save to db

--- src/graphql/queries/guild/commands.js
+++ src/graphql/queries/guild/commands.js
@@ -3,22 +3,24 @@ import gql from "graphql-tag";
 export default gql`
   query guild($guildId: ID) {
     guild(id: $guildId) {
-      commands {
-        name
-        msgDelete
-        permission {
-          disabled
-          disabledChannels {
-            id
-          }
-          disabledRoles {
-            id
-          }
-          enabledChannels {
-            id
-          }
-          enabledRoles {
-            id
+      settings {
+        commands {
+          name
+          msgDelete
+          permission {
+            disabled
+            disabledChannels {
+              id
+            }
+            disabledRoles {
+              id
+            }
+            enabledChannels {
+              id
+            }
+            enabledRoles {
+              id
+            }
           }
         }
       }
